6404833d37ec6ba9c02e8e61d8b98fcee1300bf4
suyashgh <133814945+suyashghgit@users.noreply.github.com>
2025-08-07 13:17:12 -0500

    Update docs.sh

--- docs.sh
+++ docs.sh
@@ -20,28 +20,4 @@ case "$1" in
         mkdocs gh-deploy
         echo "Documentation deployed to GitHub Pages"
         ;;
-    "gh-deploy")
-        echo "Deploying to GitHub Pages (manual method)..."
-        echo "This will push the built site to the 'gh-pages' branch"
-        mkdocs gh-deploy --force
-        echo "Documentation deployed to GitHub Pages"
-        echo "Visit: https://suyashghgit.github.io/zendesk-cx-bot/"
-        ;;
-    *)
-        echo "Usage: ./docs.sh [serve|build|deploy|gh-deploy]"
-        echo ""
-        echo "Commands:"
-        echo "  serve      - Start development server (http://localhost:8000)"
-        echo "  build      - Build static site in 'site/' directory"
-        echo "  deploy     - Build and deploy to GitHub Pages (manual)"
-        echo "  gh-deploy  - Deploy to GitHub Pages (manual, with force)"
-        echo ""
-        echo "Examples:"
-        echo "  ./docs.sh serve      # Start development server"
-        echo "  ./docs.sh build      # Build static site"
-        echo "  ./docs.sh deploy     # Deploy to GitHub Pages"
-        echo "  ./docs.sh gh-deploy  # Force deploy to GitHub Pages"
-        echo ""
-        echo "GitHub Pages URL: https://suyashghgit.github.io/zendesk-cx-bot/"
-        ;;
 esac 
